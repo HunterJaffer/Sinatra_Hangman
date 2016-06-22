d0ce37286c32cdc5c2b297ed475d1659cc442079
Tejas <tejassharma08@gmail.com>
2016-06-22 23:58:24 +0000

    Hangperson logic made ONLY

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -8,8 +8,63 @@ class HangpersonGame
   # def initialize()
   # end
   
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+  
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+  
+  def guess(str1)
+    raise ArgumentError if str1 == nil
+    if str1 == ''
+      raise ArgumentError.new("No blank spaces")
+    end
+    str1 = str1[/[a-zA-Z]+/]
+    raise ArgumentError.new("Only letters") if str1 == nil
+    str1 = str1.downcase
+    if(@word.include? str1)
+      if !@guesses.include? str1    
+        @guesses += str1
+      else
+        return false
+      end
+    else
+      if !@wrong_guesses.include? str1
+        @wrong_guesses += str1 
+      else
+        return false
+      end
+    end
+  end
+  
+  def word_with_guesses()
+    random_word = ""
+    0.upto(@word.size - 1) do |x|
+        random_word += "-"
+    end
+    @guesses.chars do |char|
+      0.upto(@word.size - 1) do |x|
+        if @word[x] == char
+        	random_word[x] = char
+        end
+      end
+    end
+    return random_word
+  end
+  
+  def check_win_or_lose()
+    if word_with_guesses() != @word && guesses.size() + wrong_guesses.size() >= 7
+      return :lose
+    elsif word_with_guesses() == @word
+      return :win
+    else
+      return :play
+    end
+    
   end
 
   def self.get_random_word
