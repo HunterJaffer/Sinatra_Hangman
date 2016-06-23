76ce7a77e9bce646257772e577c42f0e503d52d7
HusainJaffer <husainjaffe@gmail.com>
2016-06-23 20:23:02 +0200

    Made hangman_person.rb more readable and added Regex

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -1,14 +1,10 @@
-class HangpersonGame
+cclass HangpersonGame
 
+  attr_accessor :word,:guesses,:wrong_guesses
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
 
   # Get a word from remote "random word" service
-
-  # def initialize()
-  # end
-  attr_accessor :word,:guesses,:wrong_guesses
-
 
   def initialize(word)
     @word = word
@@ -16,47 +12,30 @@ class HangpersonGame
     @wrong_guesses = ""
   end
 
-
-  def guess(str1)
-    if str1 == "" || str1 =~/[^a-zA-Z]/ || str1 == nil
-      raise ArgumentError.new("Empty string not accepted")
+  def guess(guess)
+    raise ArgumentError if guess == '' or guess =~ /[^a-zA-Z]/ or guess.nil?
+    if !(@guesses=~/#{guess}/i) and !(@wrong_guesses=~/#{guess}/i)
+      @guesses << guess.downcase if @word=~/#{guess}/i
+      @wrong_guesses << guess.downcase if !(@word=~/#{guess}/i)
+      return true
     end
-    str1 = str1.downcase
-    if word.include? str1
-      if @guesses.include? str1
-        return false
-      else
-        @guesses << str1
-        return true
-      end
-    else
-      if @wrong_guesses.include? str1
-        return false
-      else
-        @wrong_guesses << str1
-        return true
-      end
-    end
+    return false
   end
 
   def word_with_guesses
     if @guesses.empty? and @wrong_guesses.empty?
-      word = '-'
-      1.upto(@word.length) {word<<'-'}
+      word = ''
+      1.upto(@word.length) {word<<'-' }
       return word
     end
-    return @word.gsub(/[^#{@wrong_guesses}]/,'-') if @guesses.empty?
-    @word.gsub(/[^#{@guesses}]/,'-')
+    return @word.gsub(/[^#{@wrong_guesses}]/, '-') if @guesses.empty?
+    @word.gsub(/[^#{@guesses}]/,"-")
   end
 
   def check_win_or_lose
-    if wrong_guesses.size() >= 7
-      return :lose
-    elsif word_with_guesses == @word
-      return :win
-    else
-      return :play
-    end
+    return :lose if @wrong_guesses.length >= 7
+    return :win if @word==word_with_guesses
+    return :play
   end
 
   def self.get_random_word
